d9e1c141de8966dd4d2f121c7104f2c7a36b73c4
maheshreddy415 <mahesh.parlapalli@cognizant.com>
2016-04-20 21:11:47 +0530

    Added files via upload

---
+++ helpers/imageuploader.js
@@ -0,0 +1,19 @@
+var multer  = 	require('multer');
+
+var storage = 	multer.diskStorage({
+					destination: function (req, file, cb) {
+						cb(null, './public/contactimages')
+					},
+					filename: function (req, file, cb) {
+						cb(null, Date.now() + '-' + file.originalname)
+					},
+					onFileUploadStart: function(file) {
+						if(file.mimetype !== 'image/jpg' && file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+							return false;
+						}
+					}
+				});
+
+module.exports = {
+	upload : multer({ storage: storage })
+}
